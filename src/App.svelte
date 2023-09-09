8c9f9fd6d86b544abac46b150b5856036cdc76b8
FXmario <fransiskus_mario87@outlook.com>
2023-09-09 15:07:54 +0700

    Add Models and Interface

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { ItemInterface } from './models/items/Item.interface'
+  import ItemListComponent from './components/items/ItemList.component.svelte'
+
+  const items: ItemInterface[] = [
+  {
+    id: 1,
+    name: 'Item 1',
+    selected : false
+  },
+  {
+    id: 2,
+    name: 'Item 2',
+    selected : false
+  },
+  {
+    id: 3,
+    name: 'Item 3',
+    selected : false
+  }
+]
+</script>
+
+<main>
+  <div class="home">
+      <ItemListComponent items={items}/>
+  </div>
+</main>
+
+<style>
+</style>
---
+++ src/components/items/ItemList.component.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { ItemInterface } from '../../models/items/Item.interface'
+  export let items: ItemInterface[] = []
+</script>
+
+<div>
+  <h3>Items:</h3>
+  <ul>
+    {#each items as item}
+      <li>
+        {item.name}
+      </li>
+    {/each}
+  </ul>
+</div>
+
