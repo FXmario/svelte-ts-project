98a8a976129ed66c8cf8e8960ba10f6bd69a8792
FXmario <fransiskus_mario87@outlook.com>
2023-09-09 18:11:49 +0700

    Adding Events To the Items Component

--- src/components/items/ItemList.component.svelte
+++ src/components/items/ItemList.component.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
   import type { ItemInterface } from '../../models/items/Item.interface'
   export let items: ItemInterface[] = []
+
+  const handleClick = (item: ItemInterface) => {
+    item.selected = !item.selected
+    items = items
+    console.log('handleItemClick', item.id, item.selected)
+  }
 </script>
 
 <div>
   <h3>Items:</h3>
   <ul>
     {#each items as item}
-      <li>
-        {item.name}
+      <li on:click={() => handleClick(item)}>
+        {item.name} [{item.selected}]
       </li>
     {/each}
   </ul>
